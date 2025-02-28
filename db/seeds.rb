4422a22c60600f01ea2f0e98cbb779a5dda8f6cd
Nevets10 <12600557+Nevets10@users.noreply.github.com>
2025-02-27 19:28:50 -0800

    Add seeds data and migrate/seed db

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,39 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+[
+  ['ATL', 'Hartsfield-Jackson Atlanta International Airport'],
+  ['DFW', 'Dallas-Fort Worth International Airport'],
+  ['DEN', 'Denver International Airport'],
+  ['PEK', 'Beijing Capital International Airport'],
+  ['PKX', 'Beijing Daxing International Airport'],
+  ['LHR', 'London Heathrow Airport'],
+  ['HND', 'Tokyo International Airport'],
+  ['ORD', "Chicago O'Hare International Airport"],
+  ['LAX', 'Los Angeles International Airport'],
+  ['CDG', 'Charles de Gaulle International Airport'],
+  ['CGK', 'Soekarno-Hatta International Airport'],
+  ['DXB', 'Dubai International Airport'],
+  ['FRA', 'Frankfurt Airport'],
+  ['HKG', 'Hong Kong International Airport'],
+  ['BKK', 'Suvarnabhumi Airport'],
+  ['SIN', 'Singapore Changi Airport'],
+  ['AMS', 'Amsterdam Airport Schiphol'],
+  ['JFK', 'John F. Kennedy International Airport'],
+  ['CAN', 'Guangzhou Baiyun International Airport'],
+  ['MAD', 'Madrid-Barajas Airport'],
+  ['IST', 'Istanbul Airport'],
+  ['PVG', 'Shanghai Pudong International Airport'],
+  ['SFO', 'San Francisco International Airport'],
+  ['LAS', 'Harry Reid International Airport'],
+  ['CLT', 'Charlotte/Douglas International Airport'],
+  ['PHX', 'Sky Harbor International Airport'],
+  ['IAH', 'George Bush Intercontinental Airport'],
+  ['KUL', 'Kuala Lumpur International Airport'],
+  ['MIA', 'Miami International Airport'],
+  ['ICN', 'Incheon International Airport'],
+  ['MUC', 'Munich Airport']
+].each do |airport|
+  Airport.find_or_create_by!(code: airport[0], name: airport[1])
+end
