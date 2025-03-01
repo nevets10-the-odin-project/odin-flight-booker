2db8094a8c9f657cc834753dd27526dd3d6b5849
Nevets10 <12600557+Nevets10@users.noreply.github.com>
2025-02-28 20:22:59 -0800

    Add class name to airport model for associations

--- app/models/airport.rb
+++ app/models/airport.rb
@@ -1,4 +1,4 @@
 class Airport < ApplicationRecord
-  has_many :departure_flights, inverse_of: 'departure_airport'
-  has_many :arriving_flights, inverse_of: 'arriving_airport'
+  has_many :departure_flights, inverse_of: 'departure_airport', class_name: 'Flight'
+  has_many :arriving_flights, inverse_of: 'arriving_airport', class_name: 'Flight'
 end
