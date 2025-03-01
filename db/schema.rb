aa97cfd83a319b5ecfe37ef27692885b59a2071f
Nevets10 <12600557+Nevets10@users.noreply.github.com>
2025-02-28 18:35:40 -0800

    Migrate flights into db

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_02_28_032139) do
+ActiveRecord::Schema[8.0].define(version: 2025_02_28_034158) do
   create_table "airports", force: :cascade do |t|
     t.string "code"
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  create_table "flights", force: :cascade do |t|
+    t.integer "airport_id", null: false
+    t.datetime "start"
+    t.time "duration"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["airport_id"], name: "index_flights_on_airport_id"
+  end
+
+  add_foreign_key "flights", "airports"
 end
