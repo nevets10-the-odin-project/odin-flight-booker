56f530f09764c0d5725eb9c244500204f3929792
Nevets10 <12600557+Nevets10@users.noreply.github.com>
2025-03-01 19:39:48 -0800

    Add new form to flights/index of available flights

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,4 +1,8 @@
 class Flight < ApplicationRecord
   belongs_to :departure_airport, foreign_key: 'departure_airport', class_name: 'Airport'
   belongs_to :arriving_airport, foreign_key: 'arriving_airport', class_name: 'Airport'
+
+  def format_flight_info
+    "#{departure_airport.code} to #{arriving_airport.code} | Departure time: #{start} | Duration: #{duration} hours"
+  end
 end
