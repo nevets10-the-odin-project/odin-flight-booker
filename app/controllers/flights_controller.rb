aa42c482a63d4318b1ab054a07fe0634193cf584
Nevets10 <12600557+Nevets10@users.noreply.github.com>
2025-03-03 20:11:32 -0800

    Update controller and view to handle no params

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,9 +1,10 @@
 class FlightsController < ApplicationController
   def index
+    return unless params['[departure_time(1i)]']
+
     @departure_time = Date.new(params['[departure_time(1i)]'].to_i, params['[departure_time(2i)]'].to_i,
                                params['[departure_time(3i)]'].to_i)
 
-    puts @departure_time
     @flights = Flight.where(departure_airport: Airport.where(code: params[:departure_airport]))
                      .where(arriving_airport: Airport.where(code: params[:arriving_airport]))
                      .where(start: @departure_time..@departure_time + 1.day)
