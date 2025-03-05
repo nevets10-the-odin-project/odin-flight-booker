a2fb245c2e78e4d94baaeaae640b0e595955ea92
Nevets10 <12600557+Nevets10@users.noreply.github.com>
2025-03-04 20:42:29 -0800

    Start work on posting booking records

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -8,5 +8,12 @@ class BookingsController < ApplicationController
   end
 
   def create
+    @booking = Booking.new(bookings_params)
+  end
+
+  private
+
+  def bookings_params
+    params.expect(:flight_id, passenger: [[id: %i[name email]]])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'up' => 'rails/health#show', as: :rails_health_check
-  resources :bookings, only: %i[new create]
+  get '/bookings/new', to: 'bookings#new'
+  post '/bookings/create', to: 'bookings#create'
   root 'flights#index'
 end
