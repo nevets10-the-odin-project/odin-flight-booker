0af971e7364f199db2d652aeeb6723ed353d5ccc
Nevets10 <12600557+Nevets10@users.noreply.github.com>
2025-03-05 19:57:10 -0800

    Adjust routes to include booking show

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get 'up' => 'rails/health#show', as: :rails_health_check
-  get '/bookings/new', to: 'bookings#new'
+  resources :bookings, only: %i[new show]
   post '/bookings/create', to: 'bookings#create'
   root 'flights#index'
 end
